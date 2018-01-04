c84599502e62f7441d932d1d66a6ae02ae48f20b
Dipak <dick.panch@gmail.com>
2018-01-04 21:36:33 +0530

    Rake Deflactor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.static_cache_control = "public, s-maxage=31536000, maxage=15552000"
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
